0a567286901e78040451701798b061f18d1da850
wilianx7 <wilianx7@hotmail.com>
2021-01-13 20:00:25 -0300

    Add UserDTO

---
+++ app/DTO/UserDTO.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DTO;
+
+use Illuminate\Support\Collection;
+use Spatie\DataTransferObject\DataTransferObject;
+
+class UserDTO extends DataTransferObject
+{
+    public ?int $id;
+    public string $name;
+    public string $email;
+    public string $login;
+    public ?string $password;
+
+    public static function fromCollection(Collection $data): self
+    {
+        return new self([
+            'id' => intval($data->get('id')) ?: null,
+            'name' => $data->get('name'),
+            'email' => $data->get('email'),
+            'login' => $data->get('login'),
+            'password' => $data->get('password'),
+        ]);
+    }
+}
