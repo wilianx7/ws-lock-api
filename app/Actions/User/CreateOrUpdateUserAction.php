29eabae616c6eafb6732fdf38d1a87a773bf9cfe
wilianx7 <wilianx7@hotmail.com>
2021-01-13 20:00:19 -0300

    Add CreateOrUpdateUserAction

---
+++ app/Actions/User/CreateOrUpdateUserAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Actions\User;
+
+use App\DTO\UserDTO;
+use App\Models\User;
+
+class CreateOrUpdateUserAction
+{
+    public function execute(UserDTO $userDTO): User
+    {
+        $user = User::findOrNew($userDTO->id);
+
+        if ($user->exists && !User::getAuthenticated()) {
+            abort(401, 'Unauthorized action!');
+        }
+
+        return $this->createOrUpdateUser($user, $userDTO);
+    }
+
+    private function createOrUpdateUser(User $user, UserDTO $userDTO): User
+    {
+        $user->name = $userDTO->name;
+        $user->login = $userDTO->login;
+        $user->email = $userDTO->email;
+
+        if ($userDTO->password) {
+            $user->password = bcrypt($userDTO->password);
+        }
+
+        $user->save();
+
+        return $user;
+    }
+}
