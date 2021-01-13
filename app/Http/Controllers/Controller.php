b1ac5b458daa025e4920dea5ca7e1381e627f1b9
wilianx7 <wilianx7@hotmail.com>
2021-01-13 19:54:04 -0300

    Add base methods -- Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,5 +10,32 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests;
+    use DispatchesJobs;
+    use ValidatesRequests;
+
+    protected function getIds(string $resourceName): array
+    {
+        $ids = request()->route()->parameter($resourceName);
+
+        return explode(',', $ids);
+    }
+
+    protected function find($model, string $id): Model
+    {
+        $include = $this->getInclude();
+
+        if (reset($include)) {
+            $includes = collect($this->getInclude());
+
+            return $model->with($includes->toArray())->find($id);
+        } else {
+            return $model->find($id);
+        }
+    }
+
+    private function getInclude(): array
+    {
+        return explode(',', request()->get('include') ?? '');
+    }
 }
