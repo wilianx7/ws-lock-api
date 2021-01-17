93b5df074ffc6f691e132ce6d12043bb53dd3c2e
wilianx7 <wilianx7@hotmail.com>
2021-01-17 10:38:26 -0300

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LockController;
+use App\Http\Controllers\LockHistoryController;
+use App\Http\Controllers\MQTTController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 /**
  * Public routes
@@ -24,10 +16,21 @@ Route::group(['middleware' => ['api', 'throttle:180,1']], function () {
     Route::put('users/create-or-update', [UserController::class, 'createOrUpdate']);
 });
 
+/**
+ * Private routes
+ */
 Route::group(['middleware' => ['api', 'auth:api', 'throttle:60,1']], function () {
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('validate', [AuthController::class, 'validateJwt']);
 
     Route::apiResource('users', UserController::class)->except(['store', 'update']);
+
+    Route::put('locks/create-or-update', [LockController::class, 'createOrUpdate']);
+    Route::apiResource('locks', LockController::class)->except(['store', 'update']);
+
+    Route::apiResource('lock-histories', LockHistoryController::class)->only(['index']);
+
+    Route::put('mqtt/open-door', [MQTTController::class, 'openDoor']);
+    Route::put('mqtt/close-door', [MQTTController::class, 'closeDoor']);
 });
