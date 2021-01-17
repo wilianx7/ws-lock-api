39b14f40e394323f76614315238ad911f1d55c97
wilianx7 <wilianx7@hotmail.com>
2021-01-17 11:21:26 -0300

    Create lock history when open/close -- MQTTController

--- app/Http/Controllers/MQTTController.php
+++ app/Http/Controllers/MQTTController.php
@@ -2,28 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\LockHistory\CreateLockHistoryAction;
 use App\Actions\MQTT\MQTTPublishAction;
+use App\Enums\LockStateEnum;
 use App\Http\Requests\MQTTRequest;
 use App\Models\Lock;
 
 class MQTTController extends Controller
 {
     private MQTTPublishAction $MQTTPublishAction;
+    private CreateLockHistoryAction $createLockHistoryAction;
 
-    public function __construct(MQTTPublishAction $MQTTPublishAction)
-    {
+    public function __construct(
+        MQTTPublishAction $MQTTPublishAction,
+        CreateLockHistoryAction $createLockHistoryAction
+    ) {
         $this->MQTTPublishAction = $MQTTPublishAction;
+        $this->createLockHistoryAction = $createLockHistoryAction;
     }
 
     public function closeDoor(MQTTRequest $request)
     {
         $macAddress = $request->input('mac_address');
 
-        if (!Lock::where('mac_address', $macAddress)->exists()) {
+        $lock = Lock::where('mac_address', $macAddress)->first();
+
+        if (!$lock) {
             abort(401, 'Non-existent lock!');
         }
 
         $result = $this->MQTTPublishAction->execute('CLOSE_DOOR', $macAddress);
+
+        if ($result) {
+            $lock->update([
+                'state' => LockStateEnum::LOCKED()
+            ]);
+
+            $this->createLockHistoryAction->execute($lock, 'Fechadura trancada.');
+        }
 
         return response($result ? 'success' : 'failed', 200);
     }
@@ -32,11 +48,21 @@ class MQTTController extends Controller
     {
         $macAddress = $request->input('mac_address');
 
-        if (!Lock::where('mac_address', $macAddress)->exists()) {
+        $lock = Lock::where('mac_address', $macAddress)->first();
+
+        if (!$lock) {
             abort(401, 'Non-existent lock!');
         }
 
         $result = $this->MQTTPublishAction->execute('OPEN_DOOR', $macAddress);
+
+        if ($result) {
+            $lock->update([
+                'state' => LockStateEnum::OPENED()
+            ]);
+
+            $this->createLockHistoryAction->execute($lock, 'Fechadura aberta.');
+        }
 
         return response($result ? 'success' : 'failed', 200);
     }
