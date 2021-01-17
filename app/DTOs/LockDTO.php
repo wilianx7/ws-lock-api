d623a1ffd6dfabfd5e81206407aec2df90350677
wilianx7 <wilianx7@hotmail.com>
2021-01-17 10:39:25 -0300

    Add LockDTO

---
+++ app/DTOs/LockDTO.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DTOs;
+
+use Illuminate\Support\Collection;
+use Spatie\DataTransferObject\DataTransferObject;
+
+class LockDTO extends DataTransferObject
+{
+    public ?int $id;
+    public string $name;
+    public string $mac_address;
+
+    public static function fromCollection(Collection $data): self
+    {
+        return new self([
+            'id' => intval($data->get('id')) ?: null,
+            'name' => $data->get('name'),
+            'mac_address' => $data->get('mac_address'),
+        ]);
+    }
+}
